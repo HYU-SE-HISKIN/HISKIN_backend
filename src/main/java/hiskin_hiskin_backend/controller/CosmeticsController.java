6e5338dd1b99a6bdb2026ba82d29693dda191262
ksy2653 <kim2653seok@gmail.com>
2023-11-08 16:55:23 +0900

    Crawling comment

--- src/main/java/hiskin_hiskin_backend/controller/CosmeticsController.java
+++ src/main/java/hiskin_hiskin_backend/controller/CosmeticsController.java
@@ -22,3 +22,17 @@ public class CosmeticsController {
         return new ResponseEntity<>(productInfo, HttpStatus.OK);
     }
 }
+
+/*
+selenium 라이브러리 사용방법:
+1. 크롬 버전 확인
+2. 크롬 버전이랑 일치하는 크롬드라이버 설치
+3. 크롬드라이버를 static 파일에 넣고 그 경로를 CosmeticsCrawlerService 클래스 path에 입력
+
+현재 크롤링이 잘 되는지 test 하기 위해 임의로 "닥터지" 라는 검색어를 입력했을 때 productName이랑 imageUrl이 json형식으로
+잘 출력되는지 Postman을 통해서 확인 -> 성공
+
+코드 수정 방향:
+LoggedInUserHolder를 통해 사용자가 로그인을 하면 userId를 저장해놓고 이 아이디를 통해 사용자 db를 조회하여 나온 스킨타입에 따른
+검색어를 올리브영 사이트에서 검색하여 productName과 imageUrl을 크롤링하여 프론트로 넘겨줌
+ */
