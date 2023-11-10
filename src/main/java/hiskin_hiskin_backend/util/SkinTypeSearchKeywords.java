d01c3bb80759aad13a9cf039f27f16b8aeff5c0f
ksy2653 <kim2653seok@gmail.com>
2023-11-10 15:05:24 +0900

    cosmetics search keys update

--- src/main/java/hiskin_hiskin_backend/util/SkinTypeSearchKeywords.java
+++ src/main/java/hiskin_hiskin_backend/util/SkinTypeSearchKeywords.java
@@ -1,22 +1,22 @@
 package hiskin_hiskin_backend.util;
 
 public enum SkinTypeSearchKeywords {
-    ORPT("1"),
-    ORPW("2"),
-    ORNT("3"),
-    ORNW("4"),
-    OSPT("5"),
-    OSPW("6"),
-    OSNT("7"),
-    OSNW("8"),
-    DRPT("9"),
-    DRPW("10"),
-    DRNT("11"),
-    DRNW("12"),
-    DSPT("13"),
-    DSPW("14"),
-    DSNT("15"),
-    DSNW("16");
+    ORPT("미백"),
+    ORPW("안티에이징"),
+    ORNT("피지"),
+    ORNW("보습주름"),
+    OSPT("민감"),
+    OSPW("예민"),
+    OSNT("여드름"),
+    OSNW("항산화성분"),
+    DRPT("보습"),
+    DRPW("주름"),
+    DRNT("히알루론산"),
+    DRNW("피부탄력"),
+    DSPT("색소침착"),
+    DSPW("감초 추출물"),
+    DSNT("콜레스테롤"),
+    DSNW("각질");
 
     private final String keyword;
 
