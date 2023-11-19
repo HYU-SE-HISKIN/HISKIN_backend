991678bb18ef6ad8dfcf0bbddfb294146971b628
ksy2653 <kim2653seok@gmail.com>
2023-11-19 16:34:06 +0900

    instance test

--- src/main/java/hiskin_hiskin_backend/controller/ChallengeController.java
+++ src/main/java/hiskin_hiskin_backend/controller/ChallengeController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api") // instance test
 public class ChallengeController {
     @Autowired
     private ChallengeService challengeService;
