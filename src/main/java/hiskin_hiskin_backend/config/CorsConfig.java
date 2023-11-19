852ab2af7b5db7d6b8599e93aec79dd300e443a1
ksy2653 <kim2653seok@gmail.com>
2023-11-20 01:37:40 +0900

    fix: cors code update

--- src/main/java/hiskin_hiskin_backend/config/CorsConfig.java
+++ src/main/java/hiskin_hiskin_backend/config/CorsConfig.java
@@ -1,10 +1,12 @@
 package hiskin_hiskin_backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
